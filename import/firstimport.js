be4cb76a2172c85ab38a92646b71908936884add
yehuda <yehudaperetz96@gmail.com>
2025-06-29 12:50:45 +0300

    Import and export practice,

---
+++ import/firstimport.js
@@ -0,0 +1,66 @@
+// module.exports = {
+//     printer
+// }
+
+// תרגיל 1
+// function multiply(a, b){
+//     return a * b
+// }
+// function divide(a, b){
+//     return a / b    
+// }
+// export {
+//     multiply,
+//     divide
+// }
+
+
+// תרגיל 2
+// const MAX_USERS = 100
+// const DEFAULT_ROLE = "guest";
+
+// export{
+//     MAX_USERS,
+//     DEFAULT_ROLE
+// }
+
+
+// תרגיל 3
+// class car
+// {
+//     constructor(make,model)
+//     {
+//         this.make = make
+//         this.model = model
+//     }
+//     getDetails() {
+//         return `Make: ${this.make}, Model: ${this.model}`;
+
+// }
+// }
+// export{
+//     car
+// }
+
+
+
+// תרגיל 4
+// function toUpper1(text)
+// {
+//     return text.toUpperCase();
+// }
+// function toLower1(text)
+// {
+//     return text.toLowerCase()
+// }
+// export { toUpper1, toLower1 };
+
+
+
+// תרגיל 5
+// const HOST = "localhost";
+// const PORT = 3000
+// export{
+//     HOST,
+//     PORT
+// }
---
+++ import/secendimport.js
@@ -0,0 +1,35 @@
+// const {printer} = require("./firstimport")
+// console.log(printer())
+
+
+// const tryimports = require("./firstimport")
+// tryimports.printer()
+
+
+// תרגיל 1
+// import {multiply,divide} from  "./firstimport.js"
+// console.log(multiply(5,10))
+// console.log(divide(20,5))
+
+// תרגיל 2
+// import {MAX_USERS,DEFAULT_ROLE} from "./firstimport.js"
+// console.log(MAX_USERS) 
+// console.log(DEFAULT_ROLE)
+
+
+//תרגיל 3
+// import {car} from "./firstimport.js"
+// const mycar = new car("bmw","M5")
+// console.log(mycar.getDetails())
+
+
+// תרגיל 4
+// import * as p from "./firstimport.js" 
+// console.log(p.toUpper1("hello"))
+// console.log(p.toLower1("HELLO"))
+
+//תרגיל 5
+// import { HOST as  localHost }  from  "./firstimport.js"
+// console.log(localHost)
+// import { PORT as serverPort } from "./firstimport.js"
+// console.log(serverPort)
